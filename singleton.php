d6d2c3a03670d7ac5e9c1eb01fd016db46be6557
Hazrat Ali <iloveyii@yahoo.com>
2016-07-30 17:54:52 +0200

    Added singleton pattern

---
+++ singleton.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Design pattern - Singleton
+ * @author: Hazrat Ali
+ * @mail: iloveyii@yahoo.com
+ * Date: 2016-07-30
+ */
+class Translation
+{
+    private static $instance;
+    private $dir = 'tr';
+    private $language;
+    private $translations;
+
+    public function __construct($language)
+    {
+        $this->language = $language;
+        $this->readTanslationFile('string.json');
+    }
+
+    public static function getInstance($language)
+    {
+        if( ! isset(self::$instance)) {
+            self::$instance = new self($language);
+        }
+
+        return self::$instance;
+    }
+
+    public function translate($text)
+    {
+        return isset($this->translations[$text]) ? $this->translations[$text] : false;
+    }
+
+    private function readTanslationFile($fileName)
+    {
+        $filePath = $this->getPathAbs($fileName);
+
+        if(file_exists($filePath)) {
+            $contents = file_get_contents($filePath);
+        } else {
+            $contents = json_encode([
+                'language'=>'språk',
+                'code'=>$this->language,
+                'country'=>'land',
+                'city'=>'stad'
+            ]);
+
+            file_put_contents($filePath, $contents);
+        }
+
+        $this->translations = json_decode($contents, JSON_FORCE_OBJECT);
+    }
+
+    private function getPathAbs($fileName)
+    {
+        $path = sprintf("%s/%s/%s/%s", dirname(__FILE__), $this->dir, $this->language, $fileName);
+
+        if( ! file_exists(dirname($path))) {
+            mkdir(dirname($path), 0777, true);
+        }
+
+        return $path;
+    }
+}
+
+// TEST DRIVE
+
+// object way
+$translation = new Translation('sv');
+echo $translation->translate('language') . PHP_EOL;
+
+// singleton way - neat and one liner
+echo Translation::getInstance('sv')->translate('language') . PHP_EOL;
